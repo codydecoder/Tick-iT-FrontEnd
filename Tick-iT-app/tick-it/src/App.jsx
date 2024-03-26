2f89e02cbd0375324d2cb6499ad7b08164434dee
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-03-26 15:35:34 -0500

    Finished Footer

--- Tick-iT-app/tick-it/src/App.jsx
+++ Tick-iT-app/tick-it/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useState, useEffect, useContext } from 'react'
 import Main from './components/Main'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   )
 }
--- Tick-iT-app/tick-it/src/components/Search.jsx
+++
@@ -1,7 +0,0 @@
-export default function Search() {
-  return (
-    <div>
-      <h1>This is Search Component</h1>
-    </div>
-  )
-}
