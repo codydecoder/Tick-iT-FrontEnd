c85d0d9ad19aa46abda1bd99e5066c77742a536f
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-03-26 10:18:04 -0500

    Working on header

--- Tick-iT-app/tick-it/src/components/Header.jsx
+++ Tick-iT-app/tick-it/src/components/Header.jsx
@@ -1,7 +1,26 @@
 export default function Header() {
   return (
-    <div>
-      <h1>This is Header Component</h1>
+    <div className='header-container'>
+      <div id='header-left'>
+        <div className="header-dropdown">
+          Dropdown
+        </div>
+      </div>
+      <nav id='header-right'>
+        <div className="header-links">
+          VENUES
+        </div>
+        <div className="header-links">
+          EVENTS
+        </div>
+        <div className="header-links">
+          MY EVENTS
+        </div>
+      </nav>
+      <div className="header-dropdown">
+        
+      </div>
+
     </div>
   )
 }
