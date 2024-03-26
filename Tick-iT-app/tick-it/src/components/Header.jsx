d79c067a7ea6634876276c2d19ec4f47ff8c61b8
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-03-26 13:20:01 -0500

    Finished Header

--- Tick-iT-app/tick-it/src/components/Header.jsx
+++ Tick-iT-app/tick-it/src/components/Header.jsx
@@ -1,26 +1,35 @@
-export default function Header() {
-  return (
-    <div className='header-container'>
-      <div id='header-left'>
-        <div className="header-dropdown">
-          Dropdown
-        </div>
-      </div>
-      <nav id='header-right'>
-        <div className="header-links">
-          VENUES
-        </div>
-        <div className="header-links">
-          EVENTS
-        </div>
-        <div className="header-links">
-          MY EVENTS
-        </div>
-      </nav>
-      <div className="header-dropdown">
-        
-      </div>
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
+
+export default function Header() {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  return (
+    <div className="header-container">
+      <Link className="header-title">Tick-iT</Link>
+      <div className='header-top'>
+        <div id='header-left'>
+          <div className="header-dropdown" >
+            <button id="header-dropdown-button" onClick={() => setShowSearchBar(!showSearchBar)}>Search?</button>
+          
+          </div>
+        </div>
+        <nav id='header-right'>
+          <Link className="header-links" to="/venues">VENUES</Link>
+          <Link className="header-links" to="/events">EVENTS</Link>
+          <Link className="header-links" to="/myevents">MY EVENTS</Link>
+        </nav>
+      </div>
+      <div className="header-dropdown">
+        {showSearchBar && (
+          <div className="search-bar">
+            <input id="search-input" type="text" placeholder="Search..." />
+            <button id="search-submit" type="submit">Search</button>
+          </div>
+        )}
+      </div>
+    
     </div>
   )
 }
