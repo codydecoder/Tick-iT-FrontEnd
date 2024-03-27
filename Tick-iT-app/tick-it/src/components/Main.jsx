3ddbee6ec13445526031eb7b9376905590a73ceb
toddlf705 <toddlf705@gmail.com>
2024-03-27 09:43:29 -0400

    populate data for venuelist and  venuedetails

--- Tick-iT-app/tick-it/src/components/Main.jsx
+++ Tick-iT-app/tick-it/src/components/Main.jsx
@@ -1,7 +1,15 @@
+import VenueList from './VenueList'
+import VenueDetails from './VenueDetails'
+import {Routes, Route} from 'react-router-dom'
+
 export default function Main() {
   return (
     <div>
       <h1>This is Main Component</h1>
+      <Routes>
+        <Route path='/venues' element = {<VenueList/>}/>
+        <Route path='/venues/:venueId' element = {<VenueDetails/>}/>
+      </Routes>
     </div>
   )
 }
